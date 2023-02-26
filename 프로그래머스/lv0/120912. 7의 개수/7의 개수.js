b043ee39f1ddd1d5d47f6931c8b788b7f6db2e3d
onionss <110672368+onionsss11@users.noreply.github.com>
2023-02-26 17:40:54 +0900

    [level 0] Title: 7의 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,10 +1,16 @@
 function solution(array) {
-   let str =  array.toString().split(",").join("")
+        
+  return  array.join("").split("").filter((el)=>{
+         if(el ==="7") return el 
+        }).length
+}
+
+
+//    let str =  array.toString().split(",").join("")
     
-   let count = 0
-for(let i = 0 ; i < str.length; i++){
-  if(str[i].includes("7"))
-    count++
-}
-    return count
-}
+//    let count = 0
+// for(let i = 0 ; i < str.length; i++){
+//   if(str[i].includes("7"))
+//     count++
+// }
+//     return count
