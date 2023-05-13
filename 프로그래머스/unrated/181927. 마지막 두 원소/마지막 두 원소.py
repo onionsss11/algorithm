50aa4c1be23e769c3b918dc7ea00e02d51169dee
onionss <110672368+onionsss11@users.noreply.github.com>
2023-05-13 20:58:42 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -3,8 +3,11 @@ def solution(num_list):
     
     for i in range(len(num_list)):
         answer.append(num_list[i])
-    if num_list[len(num_list)-2]<num_list[len(num_list)-1]:
-        answer.append(num_list[len(num_list)-1]-num_list[len(num_list)-2])
-    elif num_list[len(num_list)-2]>=num_list[len(num_list)-1]:
-        answer.append(2*(num_list[len(num_list)-1]))
+    if num_list[-2]<num_list[-1]:
+        answer.append(num_list[-1]-num_list[-2])
+    elif num_list[-2]>=num_list[-1]:
+        answer.append(2*(num_list[-1]))
     return answer
+
+#     l.append(l[-1]-l[-2] if l[-1]>l[-2] else l[-1]*2)
+#     return l
