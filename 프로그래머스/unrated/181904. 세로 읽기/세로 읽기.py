ad3c4128285aed1b513117389bf608fcfb711f87
onionss <110672368+onionsss11@users.noreply.github.com>
2023-05-26 21:06:43 +0900

    [unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,17 +1,7 @@
 def solution(my_string, m, c):
-    answer = ''
-    word =''
-    arr = []
-    for i in my_string:
-        if len(word)<m:
-            word += i
-        else:
-            arr.append(word)
-            word = ""
-            word += i
-    arr.append(word)
-
-    for i in range(len(arr)):
-        answer += arr[i][c-1]
-            
-    return answer
+    result = ""
+    
+    for i in range(c-1, len(my_string), m):
+        result += my_string[i]
+    
+    return result
