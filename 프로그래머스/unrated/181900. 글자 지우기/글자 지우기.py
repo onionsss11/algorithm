12401205bef43113ba189df3f37f8555290fb7ce
onionss <110672368+onionsss11@users.noreply.github.com>
2023-05-28 21:02:25 +0900

    [unrated] Title: 글자 지우기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
@@ -0,0 +1,9 @@
+def solution(my_string, indices):
+    answer = ''
+    indices.sort()
+    
+    for i in range(len(my_string)):
+        if i not in indices:
+            answer += my_string[i]
+            
+    return answer
