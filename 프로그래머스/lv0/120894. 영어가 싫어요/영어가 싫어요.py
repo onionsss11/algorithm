4fad862d2e06c7056f92f95ae74f709df3494b4c
onionss <110672368+onionsss11@users.noreply.github.com>
2023-06-06 17:04:16 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,10 +1,6 @@
 def solution(numbers):
-    num = ["zero","one","two","three","four","five","six","seven","eight","nine"]
-    result = ""
-    for i in range(len(numbers)):
-        for j in range(len(num)):
-            if numbers[i:].startswith(num[j]):
-                result += str(j)
-                break
-                
-    return int(result)
+    
+    for idx, word in enumerate([ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(word,str(idx))
+        
+    return int(numbers)
