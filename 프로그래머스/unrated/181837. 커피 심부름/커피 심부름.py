8c4f26509ed37ac474235b27b4a07a3ca945b48e
onionss <110672368+onionsss11@users.noreply.github.com>
2023-06-07 14:04:04 +0900

    [unrated] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -1,11 +1,14 @@
-def solution(order):
-    price = {"americano": 4500, "cafelatte": 5000}
-    order_price = 0
-    for menu in order:
-        if "americano" in menu:
-            order_price += price["americano"]
-        elif "cafelatte" in menu:
-            order_price += price["cafelatte"]
+def solution(order):    
+    price = {"americano":4500, "cafelatte":5000}
+    
+
+    answer = 0
+    for i in order:
+        if "americano" in i:
+            answer += price["americano"]
+        elif "cafelatte" in i:
+            answer += price["cafelatte"]
         else:
-            order_price += price["americano"]
-    return order_price
+            answer += price["americano"]
+            
+    return answer
