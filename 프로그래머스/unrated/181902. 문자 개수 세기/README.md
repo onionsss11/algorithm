be5671a66a8fb0ad8bda66e38ac3bd1c219a536c
onionss <110672368+onionsss11@users.noreply.github.com>
2023-06-10 14:03:43 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/README.md
+++ 프로그래머스/unrated/181902. 문자 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 문자 개수 세기 - 181902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181902) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181902?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.98 MB, 시간: 0.08 ms
+메모리: 10 MB, 시간: 0.00 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,15 +1,9 @@
-from collections import Counter
-
-def solution(my_string):
-    counts = Counter(my_string)
-    # 대문자 A ~ Z : 아스키 코드 65 ~ 90
-    # 소문자 a ~ z : 아스키 코드 97 ~ 122
-    result = []
-    for i in range(65, 91):
-        print(i,counts[chr(i)])
-        result.append(counts[chr(i)])
-    for i in range(97, 123):
-        print(i,counts[chr(i)])
-        
-        result.append(counts[chr(i)])
-    return result
+def solution(myString):
+    answer = [0]*52
+    
+    for i in myString:
+        if i.isupper():
+            answer[ord(i)-65] += 1
+        else:
+            answer[ord(i)-71] += 1
+    return answer
