d7d5da278be6e14eaee610888bfec0b5324f7abc
onionss <110672368+onionsss11@users.noreply.github.com>
2023-06-21 19:11:49 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,10 @@
+def solution(chicken):
+    coupon = chicken
+    service = 0
+    
+    while coupon>=10:
+        service +=coupon//10
+        coupon = coupon//10 + coupon%10
+        
+    return service
+    print(coupon)
