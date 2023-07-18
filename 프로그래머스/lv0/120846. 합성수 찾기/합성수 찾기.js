d0de73b74955adf9507d96f69e52605a8f7cadd2
onionss <110672368+onionsss11@users.noreply.github.com>
2023-07-18 19:13:41 +0900

    [level 0] Title: 합성수 찾기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,18 +1,14 @@
 function solution(n) {
-    let a = 0
-    let b = 0
-    
-    for (let i = 4 ; i <=n; i++ ) {
-        for (let j =1; j<=i;j++ ) {
-
-            if (i%j===0){
-                a++
-                if(a>=3 && i==j) {
-                    b++
-                    a=0
-                }
+    let dp = new Array(n+1).fill(1)
+    for(let i = 2 ; i <= n ; i++){
+        if(dp[i]){
+            
+            for(let j = 2 ; i*j <= n ; j++){
+                dp[i*j] = 0
             }
         }
     }
-    return b
+
+    return dp.filter(el => el===0).length
+    
 }
