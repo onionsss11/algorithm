88d476b152f7d0c754738f8903d8a85bbd1f03a1
onionss <110672368+onionsss11@users.noreply.github.com>
2023-09-14 16:15:46 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 3.48 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,4 +1,7 @@
 function solution(dots) {
+    console.log(dots.sort((a,b)=>a[0] -b[0]))
+    
     dots.sort((a,b)=>a[0] - b[0])
     return Math.abs(dots[0][1] - dots[1][1]) * Math.abs(dots[1][0] - dots[2][0])
 }
+
